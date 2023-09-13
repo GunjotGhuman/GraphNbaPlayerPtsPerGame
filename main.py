ed0ebb654daff14e8622892299a47078f6f488ad
GunjotGhuman <115509778+GunjotGhuman@users.noreply.github.com>
2023-09-13 19:29:41 -0400

    Update main.py
    Added feature of average line

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from nba_api.stats.endpoints import playerdashboardbyyearoveryear
 import matplotlib.pyplot as plt
 from nba_api.stats.static import players
+import numpy as np
 
+player_dict = players.get_players()
 
+def playerExist(name):
+    playerNames = []
+    for player in player_dict:
+        playerNames.append(player['full_name'].lower())
+    if name in playerNames:
+        return True
+    return False
 
 
 def findPlayerData(name):
-    player_dict = players.get_players()
-    search_name = name
-
-    player = [player for player in player_dict if player['full_name'].lower() == search_name.lower()][0]
-
+    player = [player for player in player_dict if player['full_name'].lower() == name.lower()][0]
 
     player_id = player['id']
     player_yBy_Stats = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(player_id).get_dict()
@@ -30,10 +35,16 @@ def plotData(x, y, name):
     plt.xlabel("Years")
     plt.ylabel("Points per Game", rotation='vertical')
     plt.suptitle(name + " points per game", fontsize = 26)
+    y_mean = [np.mean(y)]*len(x)
+    mean_line = plt.plot(x, y_mean, label='Average Points Per Game', linestyle='--')
+    legend = plt.legend(loc='upper right')
     plt.show()
 
 if __name__ == '__main__':
-    player_name  = input("Please enter valid nba player: ")
+    player_name = input("Please enter valid nba player name: ")
+    while(not playerExist(player_name)):
+        player_name = input("Please enter valid nba player name: ")
+
     x,y = findPlayerData(player_name)
     plotData(x,y, player_name)
 
